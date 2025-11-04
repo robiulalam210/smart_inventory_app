39a366efaf753aac5d26d030c75a10b011eb990b
robiul <robiulstudies@gmail.com>
2025-11-04 23:37:03 +0600

    bug fix api

--- lib/feature/lab_dashboard/presentation/bloc/dashboard/dashboard_bloc.dart
+++ lib/feature/lab_dashboard/presentation/bloc/dashboard/dashboard_bloc.dart
@@ -1,9 +1,7 @@
 import 'package:smart_inventory/feature/accounts/presentation/pages/account_screen.dart';
-import 'package:smart_inventory/feature/customer/presentation/pages/create_customer_screen.dart';
 import 'package:smart_inventory/feature/expense/expense_head/presentation/pages/expense_screen.dart';
 import 'package:smart_inventory/feature/expense/presentation/pages/expense_list_screen.dart';
 import 'package:smart_inventory/feature/products/groups/presentation/pages/groups_screen.dart';
-import 'package:smart_inventory/feature/products/product/presentation/pages/product_create.dart';
 import 'package:smart_inventory/feature/products/soruce/presentation/pages/source_screen.dart';
 import 'package:smart_inventory/feature/products/unit/presentation/pages/unit_screen.dart';
 import 'package:smart_inventory/feature/purchase/presentation/page/purchase_screen.dart';
@@ -15,14 +13,10 @@ import 'package:smart_inventory/feature/report/presentation/page/sales_report_pa
 import 'package:smart_inventory/feature/report/presentation/page/stock_report_screen/stock_report_screen.dart';
 import 'package:smart_inventory/feature/report/presentation/page/top_products_screen/top_products_screen.dart';
 import 'package:smart_inventory/feature/sales/presentation/pages/pos_sale_screen.dart';
-import 'package:smart_inventory/feature/supplier/presentation/pages/create_supplierr_screen.dart';
 
 import '../../../../../core/configs/configs.dart';
 import '../../../../../core/repositories/get_response.dart';
 import '../../../../../enery_screen.dart';
-import '../../../../accounts/presentation/pages/create_account_screen.dart';
-import '../../../../common/data/models/api_response_mod.dart';
-import '../../../../common/data/models/app_parse_json.dart';
 import '../../../../customer/presentation/pages/customer_screen.dart';
 import '../../../../expense/expense_sub_head/presentation/pages/expense_sub_head_screen.dart';
 import '../../../../money_receipt/presentation/page/monery_receipt_create.dart';
@@ -43,7 +37,6 @@ import '../../../../return/sales_return/presentation/page/sales_return_page.dart
 import '../../../../sales/presentation/pages/create_pos_sale/create_pos_sale.dart';
 
 import '../../../../supplier/presentation/pages/supplier_list_screen.dart';
-import '../../../../supplier/presentation/pages/supplier_payment_create.dart';
 import '../../../../supplier/presentation/pages/supplier_payment_list_screen.dart';
 import '../../../../users_list/presentation/pages/users_screen.dart';
 import '../../../data/models/dashboard/dashboard_model.dart';
@@ -133,7 +126,7 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
         url += '?dateFilter=${event.dateFilter}';
       }
 
-      print("Fetching dashboard data from: $url"); // Debug print
+      // Debug print
 
       final res = await getResponse(
         url: url, // Use the constructed URL
@@ -151,9 +144,7 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
           responseData['message'] ?? "Failed to fetch dashboard data",
         ));
       }
-    } catch (error,st) {
-      print(error);
-      print(st);
+    } catch (error) {
       emit(DashboardError(error.toString()));
     }
   }
